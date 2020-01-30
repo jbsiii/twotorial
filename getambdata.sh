8761b9ad6f172f1ae6452cd6f348b2d2d2573c61
John Sims <john.b.sims@gmail.com>
2020-01-30 15:11:07 -0500

    Created getambdata.sh
    This is another attempt to fetch data via CURL

---
+++ getambdata.sh
@@ -0,0 +1,4 @@
+echo getting data from api
+curl https://api.ambientweather.net/v1/devices?\
+applicationKey=80845f904fbb428b9582660e20542e9eb727c1d52c724f98a6afd1a03af54103\
+&apiKey=55dcb86a4e7b4b61a179d87579486be89c86768ce5204bddac4d1cc0a8efc28c
